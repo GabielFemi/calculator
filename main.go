fd816ba6be55a31a2e9d5c52e0fc502cd432f91d
Gabriel Akinyosoye <gabrielfemi799>
2020-06-08 12:30:27 -0400

    Add Function to scan input from user

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
 	logrus.Println("Calculator App")
 
+	firstNumber := readint("Enter the first number: ")
+	secondNumber := readint("Enter the second number: ")
+	fmt.Println(firstNumber, secondNumber)
+}
+
+
+func readint(prompt string) int {
+	stdin := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt)
+	var input int
+	for {
+		_, err := fmt.Fscan(stdin, &input)
+		if err == nil && input >= 0 {
+			break
+		}
+		stdin.ReadString('\n')
+		logrus.Fatalln("Please enter a number!")
+	}
+	return input
 }
