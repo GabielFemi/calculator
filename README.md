dc197bd5293ee0e0091284f7a646b7653341d5e1
Gabriel Akinyosoye <gabrielfemi799>
2020-06-08 12:07:26 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,6 @@
+##Calculator App
+
+This is a fun Calculator App I built with Golang to learn some concepts.
+
+
+
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gabielfemi/calculator/calculator"
 	"github.com/sirupsen/logrus"
+	
 )
 
 func main() {
 	logrus.Println("Calculator App")
 	arithmetic := calculator.Arithmetic{}
 	result := arithmetic.Add(1, 3)
-	logrus.Println(result)
+	fmt.Println("The sum is", result)
 }
