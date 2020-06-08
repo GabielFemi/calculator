596d341dc9f1f98577f7c30a3e3a99062b7457c0
Gabriel Akinyosoye <gabrielfemi799>
2020-06-08 12:13:05 -0400

    Remove Hard code

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gabielfemi/calculator/calculator"
 	"github.com/sirupsen/logrus"
-	
 )
 
 func main() {
 	logrus.Println("Calculator App")
-	arithmetic := calculator.Arithmetic{}
-	result := arithmetic.Add(1, 3)
-	fmt.Println("The sum is", result)
+
 }
