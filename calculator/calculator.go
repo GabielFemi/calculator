698260f4bcf063fcac6807f9676fd13fb0bbbfc3
Gabriel Akinyosoye <gabrielfemi799>
2020-06-08 08:49:06 -0400

    Add Methods to perform arithmetic operations

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,1 +1,19 @@
 package calculator
+
+type Arithmetic struct {}
+
+func (a *Arithmetic) Add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func (a *Arithmetic) Subtract(num1 int, num2 int) int {
+	return num1 - num2
+}
+
+func (a *Arithmetic) Divide(num1 int, num2 int) int {
+	return num1 / num2
+}
+
+func (a *Arithmetic) Multiply(num1 int, num2 int) int {
+	return num1 * num2
+}
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/gabielfemi/calculator/calculator"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
 	logrus.Println("Calculator App")
+	arithmetic := calculator.Arithmetic{}
+	result := arithmetic.Add(1, 3)
+	logrus.Println(result)
 }
